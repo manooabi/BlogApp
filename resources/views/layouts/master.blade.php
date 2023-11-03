c073402ababa3d845f276ba4608abae54becdf11
manooabi <manoo.abhillash@gmail.com>
2023-11-04 02:10:02 +0530

    Categor,Blogpost,Pagination etc

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -14,9 +14,30 @@
     <!-- Styles -->
     <link href="{{asset('assets/css/styles.css')}}" rel="stylesheet">
 
+    <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.css" rel="stylesheet">
+<link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-lite.min.css" rel="stylesheet">
+
+<link href="//cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css" rel="stylesheet">
+
+<link href="https://cdn.datatables.net/1.13.6/css/dataTables.bootstrap5.min.css" rel="stylesheet">
+
+
     <!-- Scripts -->
     @vite(["resources/css/app.css", "resources/js/app.js"])
     <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
+
+    <style>
+        .dataTables_wrapper .dataTables_paginate .paginate_button
+        {
+            padding: 0px !important;
+            margin: 0px !important;
+        }
+        div.dataTables_wrapper div.dataTables_length select 
+        {
+            width: 50% !important;
+        }
+
+    </style>
 </head>
 <body>
     @include('layouts.inc.admin-navbar')
@@ -31,7 +52,28 @@
         @include('layouts.inc.admin-footer')
     </div>
     </div>
+
+
+
+    <script src=" {{asset('assets/js/jquery-3.6.0.min.js')}}"></script>
     <script src=" {{asset('assets/js/bootstrap.bundle.min.js')}}"></script>
     <script src=" {{asset('assets/js/scripts.js')}}"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-lite.min.js"></script>
+    <script>
+    $(document).ready(function() {
+        $("#mySummernote").summernote({
+            height: 150,
+        });
+        $('.dropdown-toggle').dropdown();
+    });
+</script>
+<script src="//cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.13.6/js/dataTables.bootstrap5.min.js"></script>
+<script>
+    $(document).ready( function () {
+    $('#myDataTable').DataTable();
+} );
+</script>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\DashBoardController;
+use App\Http\Controllers\Admin\PostController;
+use App\Http\Controllers\Admin\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -23,6 +26,38 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::prefix('admin')->middleware('auth','isAdmin')->group(function (){
+
 Route::get('/dashboard',[DashBoardController::class,'index']);
+Route::get('category',[CategoryController::class,'index']);
+Route::get('add-category',[CategoryController::class,'create']);
+Route::post('add-category',[CategoryController::class,'store']);
+Route::get('edit-category/{category_id}',[CategoryController::class,'edit']);
+Route::put('update-category/{category_id}',[CategoryController::class,'update']);
+Route::get('delete-category/{category_id}',[CategoryController::class,'destroy']);
+Route::get('posts',[PostController::class,'index']);
+
+ Route::get('add-post',[PostController::class,'create']);
+ Route::post('add-post',[PostController::class,'store']);
+ Route::get('post/{post_id}',[PostController::class,'edit']);
+ Route::put('update-post/{post_id}',[PostController::class,'update']);
+ Route::get('delete-post/{post_id}',[PostController::class,'destroy']);
+
+
+ Route::get('users',[UserController::class,'index']);
+
+ Route::get('user/{user_id}',[UserController::class,'edit']);
+
+
+ Route::put('update-user/{user_id}',[UserController::class,'update']);
+
+
+
+
+
+
+
+
+
+
 
 });
