d6a66de477c393f20108571b9c59f0604151cf11
manooabi <manoo.abhillash@gmail.com>
2023-11-05 18:56:37 +0530

    Typo Adjustments

--- resources/views/layouts/inc/admin-sidebar.blade.php
+++ resources/views/layouts/inc/admin-sidebar.blade.php
@@ -51,7 +51,7 @@
                     </div>
                     <div class="sb-sidenav-footer">
                         <div class="small">Logged in as:</div>
-                        Start Bootstrap
+                        Admin
                     </div>
                 </nav>
             </div>
